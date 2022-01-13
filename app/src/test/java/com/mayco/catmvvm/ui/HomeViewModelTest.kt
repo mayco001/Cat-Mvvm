a3c018eb7fc450412787e55c13cca133aadd16a9
Mayco <41454466+mayco001@users.noreply.github.com>
2022-01-13 14:17:54 -0300

    criado o test unit

--- app/src/test/java/com/mayco/catmvvm/ui/HomeViewModelTest.kt
+++ app/src/test/java/com/mayco/catmvvm/ui/HomeViewModelTest.kt
@@ -1,5 +1,62 @@
 package com.mayco.catmvvm.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.mayco.catmvvm.network.response.response.CatsResponse
+import com.mayco.catmvvm.repository.CatsRepository
+import com.nhaarman.mockitokotlin2.times
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnitRunner
+import retrofit2.Response
 
-class HomeViewModelTest
+@RunWith(MockitoJUnitRunner.Silent::class)
+@ExperimentalCoroutinesApi
+
+class HomeViewModelTest {
+
+    private val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+
+    private lateinit var viewModel: HomeViewModel
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var repository: CatsRepository
+
+    @Mock
+    private lateinit var catObserver: Observer<List<CatsResponse>>
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        Dispatchers.setMain(dispatcher)
+        viewModel = HomeViewModel((repository))
+    }
+
+    @Test
+    fun getError() = TestCoroutineDispatcher().runBlockingTest {
+        val catsResponse: List<CatsResponse> = arrayListOf()
+
+        Mockito.`when`(repository.getCats()).thenReturn(Response.success(catsResponse))
+
+        viewModel.returnApi.observeForever(catObserver)
+        viewModel.getCats()
+
+        Assert.assertNotNull(viewModel.getCats())
+    }
+}
